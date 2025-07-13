a814a3c40f707e8bd7cf1d046b67a718620aa48a
Arikara-Sudhan-M <163152087+Arikara-Sudhan-M@users.noreply.github.com>
2025-07-13 12:07:36 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,7 +18,13 @@ dotenv.config();
 const app = express();
 import path from "path";
 app.use(cors({
-  origin: "https://you-tube-pink-omega.vercel.app",
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true
 }));
 
