2a91927c9f6e20cc66e9f254c3ed0e1aebe4914d
Arikara-Sudhan-M <163152087+Arikara-Sudhan-M@users.noreply.github.com>
2025-07-13 08:44:09 +0530

    Update firebase.js

--- yourtube/src/lib/firebase.js
+++ yourtube/src/lib/firebase.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCyxbdclt2ocA5zgE-MDy1ndYIFqVMAr30",
   authDomain: "yourtube-8cda9.firebaseapp.com",
   projectId: "yourtube-8cda9",
-  storageBucket: "yourtube-8cda9.firebasestorage.app",
+  storageBucket: "yourtube-8cda9.appspot.com",
   messagingSenderId: "921641878423",
   appId: "1:921641878423:web:0d65801eebaf2b25f03ad2",
 };
