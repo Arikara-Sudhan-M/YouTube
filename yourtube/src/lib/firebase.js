9c5bc2b816aa158a01105884e819489443faebf5
Arikara-Sudhan-M <163152087+Arikara-Sudhan-M@users.noreply.github.com>
2025-07-13 08:59:03 +0530

    Update firebase.js

--- yourtube/src/lib/firebase.js
+++ yourtube/src/lib/firebase.js
@@ -1,21 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCyxbdclt2ocA5zgE-MDy1ndYIFqVMAr30",
-  authDomain: "yourtube-8cda9.firebaseapp.com",
-  projectId: "yourtube-8cda9",
-  storageBucket: "yourtube-8cda9.appspot.com",
-  messagingSenderId: "921641878423",
-  appId: "1:921641878423:web:0d65801eebaf2b25f03ad2",
+  apiKey: "AIzaSyDHldT4gTClXp3UQ4Noz_ewjLFOOdQvO_Y",
+  authDomain: "yourtube-1c629.firebaseapp.com",
+  projectId: "yourtube-1c629",
+  storageBucket: "yourtube-1c629.firebasestorage.app",
+  messagingSenderId: "460806004012",
+  appId: "1:460806004012:web:6fee4436c1d46456caa089",
+  measurementId: "G-EEB687KJB9"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
-export { auth, provider };
+const analytics = getAnalytics(app);
