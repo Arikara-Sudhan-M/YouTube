05f23f473d00e0a1715f45c56fab42bd8fa82d06
Arikara-Sudhan-M <163152087+Arikara-Sudhan-M@users.noreply.github.com>
2025-07-13 09:07:59 +0530

    Update AuthContext.js

--- yourtube/src/lib/AuthContext.js
+++ yourtube/src/lib/AuthContext.js
@@ -1,5 +1,6 @@
 import { onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth";
-import { auth, provider } from "../firebase";
+import { auth, provider } from "./firebase";
+
 
 import { useState } from "react";
 import { createContext } from "react";
